a5b2af4b45e6066c153cf3d13906990a6785b771
soliatabdullahi <soliatabdullahi8@gmail.com>
2026-08-18 15:58:04 +0100

    add parameters

--- calculate.py
+++ calculate.py
@@ -1,6 +1,17 @@
-def calculator(a, b):
-    c = a + b
+mode = input("Enter math operation(+,-,*,/) or f for celcius to fahraheit conversion : ")
+num1 = int(input("Enter first number :")) 
+if mode == 'f':
+    print(f"{num1} celsius is equivalent to {num1*9/5 + 32} fahraheit")
+else:    
+    num2 = int(input("Enter second number :"))
 
-    return c
-
-calculator(4, 7)
+    if mode == "+":
+        print(f"Answer is : {num1 + num2}")
+    elif mode == "-":
+        print(f"Answer is : {num1 - num2}")    
+    elif mode == "*":
+        print(f"Answer is : {num1 * num2}")   
+    elif mode == "/":
+        print(f"Answer is : {num1 / num2}")   
+    else:
+        print("input error")    
